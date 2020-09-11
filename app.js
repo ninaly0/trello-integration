dd6137ae2d7a6f81123461d9ffd174db4a8ab18d
Na Ly <na-ly@live.fr>
2020-09-11 15:06:15 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -57,20 +57,25 @@ listForm.addEventListener('submit', function(event){
 
     // création de Carte
     var newArticle = document.createElement('article');
+    newArticle.classList.add('to_do');
     var newArticleHeader = document.createElement('div');
-    newArticleHeader.classList.add('header_article');
+    newArticleHeader.classList.add('article_header');
     var newArticleTitle = document.createElement('h2');
     newArticleTitle.textContent = input.value;
     var newArticleCloseBtn = document.createElement('button');
     newArticleCloseBtn.textContent = 'X';
     newArticleCloseBtn.setAttribute('type', 'button');
+    newArticleCloseBtn.classList.add('close_article');
     var newArticleList = document.createElement('ul');
     var newArticleForm = document.createElement('form');
     var newArticleInput = document.createElement('input');
+    var newBtnWrapper = document.createElement('div')
     var newArticleBtn = document.createElement('button');
     newArticleBtn.setAttribute('type', 'submit');
+    newArticleBtn.classList.add('btn_add_card');
     var newArticleBtnSpan = document.createElement('span');
     newArticleBtnSpan.textContent = 'Ajouter une carte';
+    var newArticleCardBtn = document.createElement('button');
 
     newArticleForm.addEventListener('submit', function(event){
         event.preventDefault();
@@ -131,8 +136,6 @@ listForm.addEventListener('submit', function(event){
         });
 
         var cardListLinkArray = document.querySelectorAll('li a');
-        console.log(cardListLinkArray);
-        console.log(cardListLink);
 
         //En tapant sur la touche entrée, ca change les titres de la modal et de la carte
         for (var i = 0; i < cardListLinkArray.length; i++){
@@ -140,29 +143,6 @@ listForm.addEventListener('submit', function(event){
             appearTitle();
             });
         }
-            
-                // modalTextArea.addEventListener('keyup', function(event){
-                //     if(event.keyCode == 13){
-                //     // texte du lien a
-                //     cardListLink.textContent = modalTitle.textContent;
-                //     modalTextArea.value = modalTitle.textContent;
-                //     }
-                // });
-
-
-        // modalTextArea.addEventListener('keyup', function(event){
-        //     event.preventDefault();
-        //     if(event.keyCode == 13){
-        //         appearTitle();
-        //         var newModalTitle = modalTextArea.value;
-        //         modalTextArea.value = newModalTitle;
-        //         modalTitle.textContent = newModalTitle;
-        //         cardListLink.innerHTML = newModalTitle;
-        //     };
-        // });
-
-
-
 
         //définit la hauteur du textarea en fonction du texte tapé
         modalTextArea.addEventListener('input', function(){
@@ -200,14 +180,18 @@ listForm.addEventListener('submit', function(event){
     newArticle.appendChild(newArticleList);
     newArticle.appendChild(newArticleForm);
     newArticleForm.appendChild(newArticleInput);
-    newArticleForm.appendChild(newArticleBtn);
+    newArticleForm.appendChild(newBtnWrapper);
+    newBtnWrapper.appendChild(newArticleBtn)
     newArticleBtn.appendChild(newArticleBtnSpan);
+    newBtnWrapper.appendChild(newArticleCardBtn);
 
     this.parentElement.insertAdjacentElement('beforebegin', newArticle);
     this.parentElement.classList.remove('is-active');
     addBtn.classList.remove('is-hidden')
     newArticleBtnSpan.insertAdjacentHTML('beforebegin', '<ion-icon name="add-outline"></ion-icon>');
-
+    newArticleBtnSpan.classList.add('usual-icon');
+    newArticleCardBtn.insertAdjacentHTML('afterbegin', '<ion-icon name="card-outline"></ion-icon>');
+    newArticleCardBtn.classList.add('card-icon');
 
 
     input.value = '';
